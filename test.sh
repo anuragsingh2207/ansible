d0526078da4628774ca2fd1ef00212e36f324221
risingskyscrapper <risingskyscrapper@gmail.com>
2021-05-09 20:35:00 +0530

    Updated the test.sh file with output redirection

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 echo "Display OS release"
-cat /etc/redhat-release
+cat /etc/redhat-release >/tmp/test_output
 echo "Display Uptime"
-uptime
+uptime >>/tmp/test_output
 echo "Display all disk partitions"
-lsblk
+lsblk >>/tmp/test_output
 echo "Display mounted filesystems"
-df -h
+df -h >>/tmp/test_output
 echo "Running iostat command to capture IO performance stats"
-iostat -xtc 1 5 
+iostat -xtc 1 5  >>/tmp/test_output
 echo "Showing free memory in MB"
-free -m
+free -m >>/tmp/test_output
