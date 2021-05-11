e88c50dfeb0e1fe12694811482e2049db5dd8300
risingskyscrapper <risingskyscrapper@gmail.com>
2021-05-11 20:17:38 +0530

    Added play3.yml and modified test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,17 @@
 echo "Display OS release"
 cat /etc/redhat-release >/tmp/test_output
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Display Uptime"
 uptime >>/tmp/test_output
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Display all disk partitions"
 lsblk >>/tmp/test_output
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Display mounted filesystems"
 df -h >>/tmp/test_output
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Running iostat command to capture IO performance stats"
 iostat -xtc 1 5  >>/tmp/test_output
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Showing free memory in MB"
 free -m >>/tmp/test_output
